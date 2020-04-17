720cf7609486db8915065289b706e20e8a63a5a7
Andrea Batch <andrea.julca@gmail.com>
2020-04-17 17:24:56 -0400

    update server function fix

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ function pullHandler(req, res) {
   function puts(error, stdout, stderr) { sys.puts(stdout) }
     
   var exec = require('child_process').exec;
-  exec("cd /usr/GitHub/uxSense && git reset --hard HEAD && git pull", puts);
+  exec("cd /usr/GitHub/uxSense && git stash && git reset --hard HEAD && git pull", puts);
   res.redirect('/');
 }
   
